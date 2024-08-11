0bc7c5179fc56cc64ac04ee6e9ccdcc91dbe6ce2
ReturnXin <1392008423@qq.com>
2024-08-11 22:11:07 +0800

    feat:设置初始pawn类

---
+++ Source/CoinCollector/BasePlayer.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePlayer.h"
+
+// Sets default values
+ABasePlayer::ABasePlayer()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// create a component of each type
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	// set up the hierarchy
+	RootComponent = Mesh;
+	SpringArm->SetupAttachment(Mesh);
+	Camera->SetupAttachment(SpringArm);
+}
+
+// Called when the game starts or when spawned
+void ABasePlayer::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABasePlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABasePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
